96e5a5c5f5b16c1dbce1e128ad5dfc5d2d1a7a9f
Subhash Chandra <subhashbhaaratha@Suryas-MacBook-Air.local>
2025-04-05 00:31:54 +0100

    Added DB connection utility class

---
+++ src/main/java/utils/DBConnection.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/EventManagement";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new SQLException("Database connection failed!", e);
+        }
+    }
+}
